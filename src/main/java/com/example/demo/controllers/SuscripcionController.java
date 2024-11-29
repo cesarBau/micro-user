c329dee4887be9571e9844a46473d4f6c410b6da
Cesar Bautista <68753523+cesarBau@users.noreply.github.com>
2024-11-28 23:05:07 -0600

    se agrego ajuste para consumo en docker

--- src/main/java/com/example/demo/controllers/SuscripcionController.java
+++ src/main/java/com/example/demo/controllers/SuscripcionController.java
@@ -20,8 +20,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
 
 
-
-
 @RestController
 @RequestMapping("/suscription")
 public class SuscripcionController {
